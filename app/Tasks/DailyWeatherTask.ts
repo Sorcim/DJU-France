e1a13ce331d73365a404ebedf6e739156ae6c8f1
Johan PESQUER <johan@pesquer.fr>
2023-02-10 12:15:54 +0100

    End of test

--- app/Tasks/DailyWeatherTask.ts
+++ app/Tasks/DailyWeatherTask.ts
@@ -6,7 +6,7 @@ import { DateTime } from "luxon"
 
 export default class DailyWeatherTask extends BaseTask {
   public static get schedule() {
-    return "0 * * * * *"
+    return "* 0 */1 * * *"
   }
 
   /**
@@ -48,7 +48,8 @@ export default class DailyWeatherTask extends BaseTask {
           }
         })
         .catch(error => {
-          console.log(error)
+          // @ts-ignore
+          this.logger.error(error)
         })
     })
   }
